c8074dccc1c6dd78bc5f74c99127d1c64f28d19f
kdsgood12 <kdsgood12@daum.net>
2020-04-08 17:22:16 +0900

    Updates

---
+++ precache-manifest.af8a94362e7e9b154d887473cd3d15af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121994654e42f16b9245402c3ae6bf5a",
+    "url": "/dsflix/index.html"
+  },
+  {
+    "revision": "523e44333420988d7554",
+    "url": "/dsflix/static/js/2.61d4f7ca.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/dsflix/static/js/2.61d4f7ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7172a69654ece21f646",
+    "url": "/dsflix/static/js/main.309a95b9.chunk.js"
+  },
+  {
+    "revision": "0fd7ea9639edde29c3c3",
+    "url": "/dsflix/static/js/runtime-main.3ba0bee3.js"
+  },
+  {
+    "revision": "3d035a553db5cc6235c369217aa9435f",
+    "url": "/dsflix/static/media/loading.3d035a55.gif"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/dsflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DsFilx/precache-manifest.747650ebcc0427469b3d9ed2aede6d81.js"
+  "/dsflix/precache-manifest.af8a94362e7e9b154d887473cd3d15af.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DsFilx/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dsflix/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
